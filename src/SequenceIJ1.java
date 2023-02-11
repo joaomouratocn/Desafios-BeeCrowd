a2537e8c7e43183ec3143b35b06d7a4fe482aa82
joao mourato <joaomouratocn@gmail.com>
2023-02-11 09:26:24 -0300

    Sequence IJ 1

---
+++ src/SequenceIJ1.java
@@ -0,0 +1,12 @@
+public class SequenceIJ1 {
+    public static void main(String[] args) {
+        int i = -2;
+        int j = 65;
+
+        for (int p = 1; p <= 13 ; p ++) {
+            i += 3;
+            j -= 5;
+            System.out.println("I="+ i + " J=" + j);
+        }
+    }
+}
