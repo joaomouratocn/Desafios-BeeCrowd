694f13940b2b55fff26310d4a764effaf1301d73
joao mourato <joaomouratocn@gmail.com>
2023-02-20 16:38:55 -0300

    DividingXbyY

---
+++ src/DividingXbyY.java
@@ -0,0 +1,25 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DividingXbyY {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.0");
+        Scanner input = new Scanner(System.in);
+
+        int totTest = input.nextInt();
+
+        for (int i = 0; i < totTest; i++) {
+            double x = input.nextInt();
+            double y = input.nextInt();
+
+            if (y == 0){
+                System.out.println("divisao impossivel");
+            }else {
+                double result = x / y;
+                System.out.println(df.format(result));
+            }
+        }
+    }
+}
