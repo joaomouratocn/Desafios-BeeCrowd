03d456bf9d559f5c7b07a8d51f2f260121f75d66
joao mourato <joaomouratocn@gmail.com>
2023-02-25 16:28:36 -0300

    RestOfaDivision

---
+++ src/RestOfaDivision.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RestOfaDivision {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+
+        if (x > y){
+            for (int i = y + 1; i < x; i++) {
+                if (i % 5 == 2 || i % 5 ==3){
+                    System.out.println(i);
+                }
+            }
+        }else {
+            for (int i = x + 1; i < y; i++) {
+                if (i % 5 == 2 || i % 5 == 3){
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
