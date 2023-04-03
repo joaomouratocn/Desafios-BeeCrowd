6dced24cd96fdfc4a72db5570474424a85f468f7
joao mourato <joaomouratocn@gmail.com>
2023-04-03 17:53:34 -0300

    BelowTheSecundaryDiagonal

---
+++ src/BelowTheSecundaryDiagonal.java
@@ -0,0 +1,26 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class BelowTheSecundaryDiagonal {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.0");
+        Scanner input = new Scanner(System.in);
+        double sum = 0;
+        char O = input.next().toUpperCase().charAt(0);
+        double[][] M = new double[12][12];
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++) {
+                M[i][j] = input.nextDouble();
+            }
+        }
+
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++) {
+                if (j > M.length-i-1) sum += M[i][j];
+            }
+        }
+
+        if (O == 'M') sum /= (double) ((M.length * M.length) - 12) / 2; // 66
+        System.out.println(df.format(sum));
+    }
+}
